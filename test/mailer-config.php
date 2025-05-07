a3c6eadb7789e1c7724a636389bce6a571fb5604
Andralis25 <andralis25@yahoo.com>
2025-05-07 09:47:12 +0300

    Update mailer-config.php

--- test/mailer-config.php
+++ test/mailer-config.php
@@ -1,1 +1,18 @@
-<conținut placeholder>
+<?php
+function send_confirmation_email($serviciu, $nume, $email, $telefon, $data, $ora, $observatii) {
+  $to = $email;  // Trimite către client
+  $subject = "Confirmare programare Andralis";
+  $message = "Bună, $nume!\n\n"
+    . "Programarea ta a fost înregistrată:\n"
+    . "Serviciu: $serviciu\n"
+    . "Data: $data\n"
+    . "Ora: $ora\n"
+    . "Observații: $observatii\n\n"
+    . "Ne vedem la salonul Andralis!";
+
+  $headers = "From: andralis25@gmail.com";
+
+  // Trimite email
+  mail($to, $subject, $message, $headers);
+}
+?>
