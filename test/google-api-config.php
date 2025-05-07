83fa10612f888408f405dac958126a0a7a6d20b0
Andralis25 <andralis25@yahoo.com>
2025-05-07 09:46:51 +0300

    Update google-api-config.php

--- test/google-api-config.php
+++ test/google-api-config.php
@@ -1,1 +1,9 @@
-<conținut placeholder>
+<?php
+function create_calendar_event($serviciu, $nume, $email, $telefon, $data, $ora, $observatii) {
+  // Aici doar pregătim datele (integrarea reală o facem după ce legăm API-ul Google)
+
+  // Exemplu simplu - doar simulare momentan
+  // În viitor adăugăm codul real Google Calendar API
+  return true;
+}
+?>
